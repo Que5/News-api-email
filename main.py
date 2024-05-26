8b09a78267cdeea0f35d4a02efe348f3fd77c98c
Que5 <phumlanibrightdlamini@gmail.com>
2024-05-26 10:36:05 +0200

    Added links, language, subject and dynamic topic

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import requests
 from send_email import send_email
 
 
+topic = "cryptocurrency"
+
+
 API_KEY = "962db8ac56fd4382b50a904a263aa1f2"
-url = "https://newsapi.org/v2/everything?q=tesla&"\
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
      "from=2024-04-25&sortBy=publishedAt&apiKey="\
-     "962db8ac56fd4382b50a904a263aa1f2"
+     "962db8ac56fd4382b50a904a263aa1f2&language=en"
 
 #Make request
 request = requests.get(url)
@@ -16,8 +20,11 @@ content = request.json()
 #Access the article titles and description
 
 body = " "
-for article in content["articles"]:
-    body = body + str(article["title"]) + "\n" + str(article["description"]) + 2*"\n"
+for article in content["articles"][:10]:
+    body =  "Subject: Today's news" + "\n" \
+    + body + str(article["title"]) + "\n" \
+        + str(article["description"]) \
+              + "\n" + str(article["url"])  + 2*"\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
